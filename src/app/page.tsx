23d53e734c1f3e41f34aef30878b06b02e9f0bde
PaulHaze <dev@artemist.com.au>
2025-02-12 12:43:23 +1100

    add screen sizes to spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center pt-10">
-      <h2 className="mt-10 pt-0 text-center leading-normal font-bold">
+    <div className="flex flex-col items-center p-4 pt-10">
+      <h1 className="mt-10 pt-0 text-center leading-normal font-bold">
         TW4/DaisyUI 5 Next Starter
-      </h2>
+      </h1>
+      <div className="w-smd h-40 bg-green-500"></div>
     </div>
   );
 }
