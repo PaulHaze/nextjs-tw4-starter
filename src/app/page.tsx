907593d43d0d9fc67aa814fdbce22b68bcea0c7f
PaulHaze <dev@artemist.com.au>
2025-02-12 16:56:15 +1100

    add dasiy config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center p-4 pt-10">
-      <h1 className="mt-10 pt-0 text-center leading-normal font-bold">
+    <div className="flex min-h-screen flex-col items-center p-4 pt-10">
+      <h1 className="pt-0 text-center leading-normal font-bold">
         TW4/DaisyUI 5 Next Starter
       </h1>
+      <button className="btn">CLICK</button>
     </div>
   );
 }
